04a70e494177b7ffbd80c906d61f2cd2fcd8a800
chenyyu <chenyyu@paypal.com>
2024-08-07 14:57:42 +0800

    Add; generate scripts

---
+++ ollama_commit_message/generate_commit_message.py
@@ -0,0 +1,91 @@
+from ollama import Client
+import subprocess
+import os
+import argparse
+
+
+def get_staged_diff() -> str:
+    try:
+        result = subprocess.run(
+            ["git", "diff", "--cached", "--no-color", "--text", "--diff-filter=AMCR"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            check=True,
+        )
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        print(f"Error while getting staged diff: {e.stderr}")
+        return ""
+
+
+def get_staged_commit_message():
+    git_dir = os.path.join(os.getcwd(), ".git")
+    commit_msg_path = os.path.join(git_dir, "COMMIT_EDITMSG")
+
+    try:
+        with open(commit_msg_path, "r") as f:
+            commit_message = f.read()
+        return commit_message
+    except FileNotFoundError:
+        print("No staged commit message found.")
+        return None
+    except Exception as e:
+        print(f"Error reading commit message: {e}")
+        raise e
+
+
+def write_commit_message(commit_message):
+    git_dir = os.path.join(os.getcwd(), ".git")
+    commit_msg_path = os.path.join(git_dir, "COMMIT_EDITMSG")
+
+    try:
+        # 写入 commit message
+        with open(commit_msg_path, "w") as f:
+            f.write(commit_message)
+        print("Commit message written successfully.")
+    except Exception as e:
+        print(f"Error writing commit message: {e}")
+        raise e
+
+
+def generate_commit_message(host: str, model: str):
+    client = Client(host=host)
+    diff_file = get_staged_diff()
+    user_commit_message = get_staged_commit_message()
+    prompt = f"""
+    You are a good git commit message writer! commit messages based on the following diff:
+    {diff_file}
+    Don't modify the user's commit message, just continue writing.
+    """
+    resp = client.chat(
+        model=model,
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": f"{user_commit_message}"},
+        ],
+    )
+    print(resp["message"]["content"])
+
+
+def main():
+    parser = argparse.ArgumentParser(description="use ollama write git commit message")
+
+    parser.add_argument(
+        "--model", "-m", type=str, default="", help="ollama model name", required=True
+    )
+    parser.add_argument(
+        "--host",
+        "-h",
+        type=str,
+        default="http://localhost:11434",
+        help="ollama host address",
+    )
+
+    args = parser.parse_args()
+
+    generate_commit_message(args.host, args.model)
+
+
+if __name__ == "__main__":
+    main()
